4a61f7463cc3fa8fbbbae6a36c60a9de77d5bcc0
dk00000000 <51994906+dk00000000@users.noreply.github.com>
2019-09-30 14:55:39 +0530

    Add files via upload

---
+++ api/Allmasterdata.php
@@ -0,0 +1,138 @@
+<?php
+session_start();
+header('Content-type: application/json');
+require_once( "../dashboard.php" );
+require_once( "../query.php" );
+class AllMasterData {
+	 
+   private $lgs,$qryObj,$dsbObj,$rfObj,$xmlObj,$conn,$stid,$prefetch;
+   function __construct() {	
+        $filepath = '../util/readquery/general/DBINFO.ini';
+        $ini_array = parse_ini_file($filepath, true);
+        $dbid='MAIN';
+        $host = $ini_array[$dbid]['HOSTIP'];
+		$service_name = $ini_array[$dbid]['SERVICENAME'];
+		$compCd = $ini_array[$dbid]['COMPCODE'];
+		$server = $ini_array[$dbid]['SERVER'];
+
+		define('DATABASE',"(DESCRIPTION =
+					(ADDRESS_LIST =
+						(ADDRESS = 
+							(PROTOCOL = TCP)(HOST = ".$host.")(PORT = 1521))
+			   			)
+					(CONNECT_DATA =	(SERVER = DEDICATED)
+					(SERVICE_NAME = ".$service_name.")
+				)				
+	  	  )");
+
+        $_SESSION['USER'] =strtoupper($_REQUEST['username']);
+        $password =strtoupper($_REQUEST['password']);
+        $_SESSION['IDTTY'] =$password.strrev($password);
+        
+        if(isset($_SESSION['USER']) && isset($_SESSION['IDTTY'])){
+       
+        $this->conn = @oci_pconnect($_SESSION['USER'], $_SESSION['IDTTY'], DATABASE, AL32UTF8);        
+			if (!$this->conn) {
+			   $m = oci_error();
+			   //echo $m['message'];
+			   echo json_encode(array('Success'=>'TRUE','Message'=>'Login Fail'));
+			   exit;
+			}else {
+				//echo json_encode(array('Success'=>'true','Message'=>'Login Success'));
+			}
+		}
+
+    }
+
+    public function getAllmasterData($data)
+	{
+		$dsbObj = new Dashboard();
+		$qry = new Query();
+		$res=array();
+
+		$oldFilter=array(':PUSER_CODE');
+	    $newFilter=array($_SESSION['USER']);
+
+
+		$filepath = 'ini/harvesting_slip.ini';
+        $queries = parse_ini_file($filepath, true);
+
+        //FOR JSON TO ARRAY
+		$data =json_decode($data, true);		
+        
+        $newSectonRes = array();
+        foreach ($data['AllMasters'][0] as $key => $value) 
+        {
+			$last_key = $key;
+			if($value==''){
+				$sync_date = '01/01/0001';
+				
+			}else{
+				$date =explode(" ", $value); //'01/01/0001';//$value;
+				$sync_date = $date[0];
+			}
+
+			$oldFilter = array(':PSYNC_DATE');
+			$newFilter = array($sync_date);
+			$newSectonQry = $qry->fetchQuery($filepath,'Q001',$key,$oldFilter,$newFilter);
+			//echo "Qry :".$newSectonQry;
+
+			$newSectonRes = $dsbObj->getData($newSectonQry);
+			$result[$key]['NEW']=$newSectonRes;
+			//exit(0);
+		}
+
+		    $fregDelQry = $qry->fetchQuery($filepath,'Q001','FREG_DELTED',$oldFilter,$newFilter);
+			$fregDelRes = $dsbObj->getData($fregDelQry);
+			$result['FREG_DELETED']['DELETED']=$fregDelRes;
+
+			$hsDelQry = $qry->fetchQuery($filepath,'Q001','HS_DELTED',$oldFilter,$newFilter);
+			$hsDelRes = $dsbObj->getData($hsDelQry);
+			$result['HS_DELETED']['DELETED']=$hsDelRes;
+
+			$fsvDelQry = $qry->fetchQuery($filepath,'Q001','FSV_DELTED',$oldFilter,$newFilter);
+			$fsvDelRes = $dsbObj->getData($fsvDelQry);
+			$result['FSV_DELETED']['DELETED']=$fsvDelRes;
+	
+		$result['Success']="true";
+	    /*FOR SECMAST MAST*/
+	    echo json_encode($result);
+        die;      		
+	}
+}
+
+
+$obj = new AllMasterData();
+$data=$_REQUEST['data'];
+$obj ->getAllmasterData($data);
+
+/*
+http://203.127.5.7/tndsugar/api/Allmasterdata.php?username=kadamkg&password=k&data={"AllMasters":[
+{
+"SECMAST":"",
+"BACK_DATA":"",
+"CANEDTMAST":"",
+"CANEQMAST":"",
+"CANETMAST":"",
+"CANEVMAST":"",
+"FIELDREG":"",
+"GADIMAST":"",
+"HSPRGMAST":"",
+"IRRMTDMAST":"",
+"IRRSRCMAST":"",
+"JOBTMAST":"",
+"PLNTMMAST":"",
+"PRTMAST":"",
+"SERIESHS_SSEGMAST":"",
+"SSEGMAST":"",
+"SSNMAST":"",
+"SVRMAST":"",
+"VHLTMAST":"",
+"VLGMAST":"",
+"V_VEHICLE_JOINT":""
+}
+]
+}
+
+*/
+?>
---
+++ api/wsInfo.php
@@ -0,0 +1,42 @@
+<?php
+// Include all configurations
+header('Content-type: application/json');
+//require_once('../dashboard.php');
+require_once( "../dashboard.php" );
+$qryObj = new Query();
+$dsbObj = new Dashboard();
+$qryPath = "ini/web_service.ini";
+
+$oldFilter=array(':PTXN_SEASON',':PPRT_CODE');
+$newFilter=array($_REQUEST['season'],$_REQUEST['farmer']);
+$getFieldRegQry = $qryObj->fetchQuery($qryPath,'Q001','GET_WSINFO',$oldFilter,$newFilter);
+$getFieldRegRes = $dsbObj->getData($getFieldRegQry);
+echo json_encode($getFieldRegRes);
+
+exit();
+           
+          /* if(sizeof($getcontractRes) > 0 ){
+           	  foreach ($getcontractRes as $key => $Row) {
+           	  	$Contractors[$key]["ComboField"] = $Row["CONTRACTORNAME"].'-'.$Row["CONTRACTORCODE"].'-'.$Row["SUBCONTRACTORNAME"].'-'.$Row["SUBCONTRACTORCODE"].'-'.$Row["VEHICLETYPE"].'-'.$Row["TXN_VHNO"];
+            	$Contractors[$key]["SequenceNo"] = $Row["SEQUENCENO"];
+            	$Contractors[$key]["Field1"]["DisplayName"] = "Contractor";
+				$Contractors[$key]["Field1"]["Value"] = $Row["CONTRACTORNAME"];
+				$Contractors[$key]["Field2"]["DisplayName"] = "Contractor Code";
+				$Contractors[$key]["Field2"]["Value"] = $Row["CONTRACTORCODE"];
+				$Contractors[$key]["Field3"]["DisplayName"] = "Sub Contractor";
+				$Contractors[$key]["Field3"]["Value"] = $Row["SUBCONTRACTORNAME"];
+				$Contractors[$key]["Field4"]["DisplayName"] = "Sub Contractor Code";
+				$Contractors[$key]["Field4"]["Value"] = $Row["SUBCONTRACTORCODE"];
+				$Contractors[$key]["Field5"]["DisplayName"] = "Vehicle Type";
+				$Contractors[$key]["Field5"]["Value"] = $Row["VEHICLETYPE"];
+				$Contractors[$key]["Field6"]["DisplayName"] = "Vehicle No.";
+				$Contractors[$key]["Field6"]["Value"] = $Row["TXN_VHNO"];
+				$Contractors[$key]["Photo"] = $Row["PHOTO"];
+				$Contractors[$key]["FingerPrint"] = $Row["FINGERPRINT"];
+              }
+              echo json_encode(array('Success'=>'true','Message'=>'Contractors available.','Contractors'=>$Contractors));
+           }else{
+              echo json_encode(array('Success'=>'true','Message'=>'Contractors not available.'));
+           }*/
+
+?>
